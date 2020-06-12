2a660018c6c64808ba57e2b78a12b5dd04cc7032
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2020-06-12 23:06:08 +0300

    add sample JavaFX app

--- desktop/src/main/java/kma/cs/sample/desktop/Main.java
+++ desktop/src/main/java/kma/cs/sample/desktop/Main.java
@@ -1,5 +1,28 @@
 package kma.cs.sample.desktop;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 
 }
