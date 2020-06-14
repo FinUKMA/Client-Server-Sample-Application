f0b870e53e55f7ac7dffc31d2682c0126959b6ed
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2020-06-14 10:11:33 +0300

    desktop app as websocket client

--- backend/src/main/java/kma/cs/sample/backend/controller/MessagingController.java
+++ backend/src/main/java/kma/cs/sample/backend/controller/MessagingController.java
@@ -8,13 +8,16 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import kma.cs.sample.domain.Message;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class MessagingController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public Message send(final Message message) {
+        log.info("Input message: {}", message);
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new Message(message.getFrom(), message.getText() + " " + time);
     }
--- desktop/src/main/java/kma/cs/sample/desktop/Main.java
+++ desktop/src/main/java/kma/cs/sample/desktop/Main.java
@@ -1,10 +1,23 @@
 package kma.cs.sample.desktop;
 
+import java.lang.reflect.Type;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import kma.cs.sample.domain.Message;
 
 public class Main extends Application {
 
@@ -23,6 +36,56 @@ public class Main extends Application {
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        stompClient.connect("ws://localhost:8080/chat", sessionHandler);
+    }
+
+    public static final class MyStompSessionHandler extends StompSessionHandlerAdapter {
+
+        @Override
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            System.out.println("New session established : " + session.getSessionId());
+            session.subscribe("/topic/messages", this);
+            System.out.println("Subscribed to /topic/messages");
+            session.send("/app/chat", getSampleMessage());
+            System.out.println("Message sent to websocket server");
+        }
+
+        @Override
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            System.out.println("Got an exception");
+            exception.printStackTrace();
+        }
+
+        @Override
+        public Type getPayloadType(StompHeaders headers) {
+            return Message.class;
+        }
+
+        @Override
+        public void handleTransportError(final StompSession session, final Throwable exception) {
+            exception.printStackTrace();
+        }
+
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            Message msg = (Message) payload;
+            System.out.println("Received : " + msg.getText() + " from : " + msg.getFrom());
+        }
+
+        /**
+         * A sample message instance.
+         * @return instance of <code>Message</code>
+         */
+        private Message getSampleMessage() {
+            return new Message("Nicky", "Howdy");
+        }
     }
 
 }
